3d1bb916dc72631bba645257778064ba81881664
Timtaran <tentaranka@gmail.com>
2024-10-21 20:06:20 +0500

    handle start

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,5 +1,6 @@
 __all__ = ["routers"]
 
 from .handle_joke_request import router as handle_joke_request_router
+from .handle_start import router as handle_start_router
 
-routers = [handle_joke_request_router]
+routers = [handle_joke_request_router, handle_start_router]
